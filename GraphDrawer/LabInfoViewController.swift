51fc1c6722536cae0bc36a04d5e727f4907543e9
Lildan <Lildan@Lildans-MacBook-Pro.local>
2017-05-13 17:32:43 +0300

    Added divided differences page

--- GraphDrawer/LabInfoViewController.swift
+++ GraphDrawer/LabInfoViewController.swift
@@ -24,11 +24,94 @@ class LabInfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.addSubview(createDividedDifferencesPageView())
     }
     
-    
-    
+    func createDividedDifferencesPageView () -> UIView {
+        let page = UIView()
+        page.frame.size = scrollView.bounds.size
+        
+        let header = UILabel()
+        header.text = "Divided Differences Table"
+        header.textAlignment = NSTextAlignment.center
+        let headerHeight : CGFloat = 35
+        header.frame = CGRect(x: 0, y: 0, width: page.bounds.width, height: headerHeight)
+        page.addSubview(header)
+        
+        let rowNumber = model.functionTabulation.count + 2
+        let columnNumber = model.dividedDifferences.count + 1
+        
+        let cellWidth = page.bounds.size.width / CGFloat(columnNumber)
+        let cellHeight = (page.bounds.size.height - headerHeight) / CGFloat(rowNumber)
+        
+        let cellBorderColor = UIColor.blue.cgColor
+        let cellBorderWidth = CGFloat(1)
+        
+        // Adding top header like: X f() f(;)...
+        for i in 0..<columnNumber {
+            let cell = createDefaultCell(cellBorderColor,cellBorderWidth)
+            cell.frame = CGRect(x: CGFloat(i)*cellWidth, y: headerHeight, width: cellWidth, height: cellHeight)
 
+            
+            // inserting appropriate text
+            if i == 0 {
+                cell.text = "X"
+            }
+            else {
+                var txt = "f("
+                for _ in 0..<(i-1) {
+                    txt += ";"
+                }
+                txt += ")"
+                cell.text = txt
+            }
+            
+            page.addSubview(cell)
+        }
+        
+        // adding column of x values
+        let yOffSet = headerHeight + cellHeight
+        for i in 0..<model.functionTabulation.count {
+            let cell = createDefaultCell(cellBorderColor,cellBorderWidth)
+            cell.frame = CGRect(x: CGFloat(0), y: yOffSet + CGFloat(i)*cellHeight, width: cellWidth, height: cellHeight)
+            cell.text = String(model.functionTabulation[i].arg)
+            page.addSubview(cell)
+        }
+        
+        let xOffSet = cellWidth
+        var addYOffSet = CGFloat(0)
+        // adding divided differences themselves into table
+        for j in 0..<model.dividedDifferences.count {
+            for i in 0..<model.dividedDifferences[j].count {
+                
+                let cell = createDefaultCell(cellBorderColor, cellBorderWidth)
+                cell.frame = CGRect(x: xOffSet + cellWidth * CGFloat(j),
+                                    y: yOffSet + cellHeight * (CGFloat(i)+addYOffSet),
+                                    width: cellWidth,
+                                    height: cellHeight)
+            
+                cell.text = String((round(100*model.dividedDifferences[j][i]))/100)
+                page.addSubview(cell)
+            }
+            addYOffSet += CGFloat(0.5)
+        }
+        
+        return page
+    }
+    
+    func createDefaultCell (_ bColor: CGColor? = nil, _ bWidth: CGFloat = CGFloat(0)) -> UILabel {
+        
+        let cell = UILabel()
+        cell.textAlignment = NSTextAlignment.center
+        cell.adjustsFontSizeToFitWidth = true
+        
+        if bColor != nil {
+        cell.layer.borderColor = bColor
+        cell.layer.borderWidth = bWidth
+        }
+        return cell
+    }
     
     private struct Storyboard {
         static let ShowGraph = "ShowGraph"
